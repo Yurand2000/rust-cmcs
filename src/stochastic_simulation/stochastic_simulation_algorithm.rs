e4ffa825a1fb0935b9397f1818637961c2a52886
Yuri Andriaccio <yurand2000@gmail.com>
2024-08-06 17:32:12 +0200

    Implement ODESimulation

--- src/stochastic_simulation/stochastic_simulation_algorithm.rs
+++ src/stochastic_simulation/stochastic_simulation_algorithm.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use ode_solvers::*;
 use rand::{Rng, SeedableRng};
 
 #[derive(Clone, PartialEq, Eq, Hash)]
@@ -75,6 +76,7 @@ impl Reaction {
 pub struct ChemicalReactionODE {
     reactions: Vec<Reaction>,
     species: HashMap<Molecule, usize>,
+    inv_species: HashMap<usize, Molecule>,
 }
 
 impl ChemicalReactionODE {
@@ -84,16 +86,24 @@ impl ChemicalReactionODE {
             .flat_map(|reaction| reaction.get_species())
             .collect();
 
-        let species = molecular_species.into_iter()
+        let species_iter: Vec<_> = molecular_species.into_iter()
             .enumerate()
             .map(|(id, mol)| (mol.clone(), id))
             .collect();
 
-        Self { reactions, species }
+        let inv_species = species_iter.iter().cloned()
+            .map(|(a, b)| (b, a)).collect();
+        let species = species_iter.into_iter().collect();
+
+        Self { reactions, species, inv_species }
     }
 
     pub fn get_species_id(&self, molecule: &Molecule) -> Option<usize> {
         self.species.get(molecule).cloned()
+    }
+
+    pub fn get_species_from_id(&self, molecule_id: &usize) -> Option<&Molecule> {
+        self.inv_species.get(molecule_id)
     }
 }
 
@@ -102,6 +112,45 @@ impl ode_solvers::System<f32, ode_solvers::DVector<f32>> for ChemicalReactionODE
         for reaction in self.reactions.iter() {
             reaction.apply_ode(&self.species, y, dy);
         }
+    }
+}
+
+use nalgebra::*;
+
+#[derive(Clone)]
+pub struct ODESimulation {
+    data: Vec<(f32, Vec<(Molecule, f32)>)>
+}
+
+impl ODESimulation {
+    pub fn new(reactions: Vec<Reaction>, mut initial_state: HashMap<Molecule, u32>, max_time: f32) -> Result<Self, String> {
+        let step_size = 0.01f32;
+        let model = ChemicalReactionODE::new(reactions);
+        let mut stepper = Dop853::new(model.clone(), 0f32, max_time, step_size, DVector::from_element(5, 0f32), 1.0e-2, 1.0e-6);
+        stepper.integrate().map_err(|err| err.to_string())?;
+
+        let data = stepper.results().to_owned();
+        let (time, molecules) = data.get();
+        let data = time.iter().cloned()
+            .zip(
+                molecules.iter().map(|molecules| molecules.iter().enumerate()
+                    .map(|(id, quantity)| {
+                        let molecule = model.get_species_from_id(&id).cloned().unwrap();
+                        (molecule, *quantity)
+                    }).collect()
+                )
+            )
+            .collect();
+
+        Ok(Self { data })
+    }
+}
+
+impl Iterator for ODESimulation {
+    type Item = (f32, Vec<(Molecule, u32)>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
     }
 }
 
