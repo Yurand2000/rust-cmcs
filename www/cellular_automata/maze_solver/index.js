68fd7abd2e400ca648d5281390ed2f8e762272e2
Yuri Andriaccio <yurand2000@gmail.com>
2024-08-18 12:37:06 +0200

    Add Animations for Maze Solver

--- www/cellular_automata/maze_solver/index.js
+++ www/cellular_automata/maze_solver/index.js
@@ -1,6 +1,13 @@
 class Model {}
 class Params {}
 
+const button_play = `<svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z" clip-rule="evenodd"/>
+</svg>`;
+const button_pause = `<svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M8 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H8Zm7 0a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1Z" clip-rule="evenodd"/>
+</svg>`;
+
 const canvas = document.createElement("canvas");
 const image = document.getElementById("image");
 const status = document.getElementById("status");
@@ -9,8 +16,13 @@ const canvas_text = document.getElementById("canvas_text");
 const maze = document.getElementById("maze");
 const step = document.getElementById("step");
 
+const rewind = document.getElementById("rewind");
+const play_pause = document.getElementById("play_pause");
+
 let chart = null;
 let model = null;
+var playing = false;
+var anim_speed = 75;
 
 /** Main entry point */
 export function main() {
@@ -31,7 +43,54 @@ function setupUI() {
     window.addEventListener("resize", setupCanvas);
     
 	maze.addEventListener("input", updateModelAndDraw);
-	step.addEventListener("input", updatePlot);
+	step.addEventListener("input", updateStep);
+
+    rewind.addEventListener("click", rewindFn);
+    play_pause.addEventListener("click", playPauseFn);
+}
+
+function rewindFn() {
+    step.value = 0;
+    updatePlayPause(false);
+    updatePlot();
+}
+
+function playPauseFn() {
+    updatePlayPause(!playing);
+}
+
+function updatePlayPauseRender() {
+    if (playing) {
+        play_pause.innerHTML = button_pause;
+        playAnimation();
+    } else {
+        play_pause.innerHTML = button_play;
+    }
+}
+
+function playAnimation() {
+    if (!playing) {
+        return;
+    }
+
+    step.value = Number(step.value) + 1;
+    updatePlot();
+
+    if (Number(step.value) < Number(step.max)) {
+        setTimeout(playAnimation, anim_speed);
+    } else {
+        updatePlayPause(false);
+    }
+}
+
+function updatePlayPause(value) {
+    playing = value;
+    updatePlayPauseRender();
+}
+
+function updateStep() {
+    updatePlayPause(false);
+    updatePlot();
 }
 
 /** Setup canvas to properly handle high DPI and redraw current plot. */
@@ -59,6 +118,7 @@ function updateModel() {
 }
 
 function updateModelAndDraw() {
+    updatePlayPause(false);
     updateModel();
     updatePlot();
 }
@@ -73,5 +133,4 @@ function updatePlot() {
     canvas_text.innerHTML = `Current Step: ${step.value}`;
     const end = performance.now();
     status.innerText = `Rendered in ${Math.ceil(end - start)}ms`;
-
 }
