9d6dab6508f8a141cb044bf2b4ea61fe69f82780
Yuri Andriaccio <yurand2000@gmail.com>
2024-09-06 18:18:07 +0200

    Update SIR pages

--- www/continuous_dynamical_systems/sir_model_birth_death/index.js
+++ www/continuous_dynamical_systems/sir_model_birth_death/index.js
@@ -8,7 +8,6 @@ const solver = document.getElementById("solver");
 
 const init_susceptible_pop = document.getElementById("init_susceptible_pop");
 const init_infected_pop = document.getElementById("init_infected_pop");
-const init_recovered_pop = document.getElementById("init_recovered_pop");
 const infection_coefficient = document.getElementById("infection_coefficient");
 const recovery_coefficient = document.getElementById("recovery_coefficient");
 const birth_rate = document.getElementById("birth_rate");
@@ -33,9 +32,8 @@ function setupUI() {
     status.innerText = "WebAssembly loaded!";
     window.addEventListener("resize", setupCanvas);
 	solver.addEventListener("input", updatePlot);
-	init_susceptible_pop.addEventListener("input", updateSusceptibleAndPlot);
-	init_infected_pop.addEventListener("input", updateInfectedAndPlot);
-	init_recovered_pop.addEventListener("input", updateRecoveredAndPlot);
+	init_susceptible_pop.addEventListener("input", updatePlot);
+	init_infected_pop.addEventListener("input", updatePlot);
     infection_coefficient.addEventListener("input", updatePlot);
 	recovery_coefficient.addEventListener("input", updatePlot);
     birth_rate.addEventListener("input", updatePlot);
@@ -56,74 +54,31 @@ function setupCanvas() {
     updatePlot();
 }
 
-function updateSusceptibleAndPlot() {
-    var susceptible = Number(init_susceptible_pop.value);
-    var infected = Number(init_infected_pop.value);
-    var recovered = Number(init_recovered_pop.value);
-
-    var extra = (susceptible + infected + recovered - 1) / 2;
-    init_infected_pop.value = (infected - extra).toString();
-    init_recovered_pop.value = (recovered - extra).toString();
-
-    normalizePops();
-    updatePlot();
-}
-
-function updateInfectedAndPlot() {
-    var susceptible = Number(init_susceptible_pop.value);
-    var infected = Number(init_infected_pop.value);
-    var recovered = Number(init_recovered_pop.value);
-
-    var extra = (susceptible + infected + recovered - 1) / 2;
-    init_susceptible_pop.value = (susceptible - extra).toString();
-    init_recovered_pop.value = (recovered - extra).toString();
-
-    normalizePops();
-    updatePlot();
-}
-
-function updateRecoveredAndPlot() {
-    var susceptible = Number(init_susceptible_pop.value);
-    var infected = Number(init_infected_pop.value);
-    var recovered = Number(init_recovered_pop.value);
-
-    var extra = (susceptible + infected + recovered - 1) / 2;
-    init_susceptible_pop.value = (susceptible - extra).toString();
-    init_infected_pop.value = (infected - extra).toString();
-
-    normalizePops();
-    updatePlot();
-}
-
-function normalizePops() {
-    var susceptible = Number(init_susceptible_pop.value);
-    var infected = Number(init_infected_pop.value);
-    var recovered = Number(init_recovered_pop.value);
-
-    var extra = (1 - susceptible - infected - recovered) / 3;
-    init_susceptible_pop.value = (susceptible + extra).toString();
-    init_infected_pop.value = (infected + extra).toString();
-    init_recovered_pop.value = (recovered + extra).toString();
-}
-
 /** Redraw currently selected plot. */
 function updatePlot() {
     status.innerText = "Rendering...";
     const start = performance.now();
+    var susceptible = Number(init_susceptible_pop.value);
+    var infected = Number(init_infected_pop.value);
+    if (susceptible + infected > 1) {
+        infected = 1 - susceptible;
+        init_infected_pop.value = String(infected);
+    }
+    var recovered = (1 - susceptible - infected);
     var params = Params.builder()
         .solver(String(solver.value))
         .max_time(Number(max_time.value))
-        .initial_susceptible_population(Number(init_susceptible_pop.value))
-        .initial_infected_population(Number(init_infected_pop.value))
-        .initial_recovered_population(Number(init_recovered_pop.value))
+        .initial_susceptible_population(Number(susceptible))
+        .initial_infected_population(Number(infected))
+        .initial_recovered_population(recovered)
         .infection_coefficient(Number(infection_coefficient.value))
         .recovery_coefficient(Number(recovery_coefficient.value))
         .birth_rate(Number(birth_rate.value));
     chart = Model.draw(canvas, params);
     canvas_text.innerHTML = `Max Time ($ t $): ${max_time.value}, ` +
-        `Initial Susceptible Pop ($ S(0) $): ${init_susceptible_pop.value}, ` + 
-        `Initial Infected Pop ($ I(0) $): ${init_infected_pop.value}, ` + 
-        `Initial Recovered Pop ($ R(0) $): ${init_recovered_pop.value}<br/>` + 
+        `Initial Susceptible Pop ($ S(0) $): ${susceptible.toFixed(2)}, ` + 
+        `Initial Infected Pop ($ I(0) $): ${infected.toFixed(2)},` + 
+        `Initial Recovered Pop ($ R(0) $): ${recovered.toFixed(2)}<br/>` + 
         `Infection Coefficient ($ \\beta $): ${infection_coefficient.value}, ` + 
         `Recovery Coefficient ($ \\gamma $): ${recovery_coefficient.value}, ` +
         `Birth Rate ($ \\mu $): ${birth_rate.value}`;
